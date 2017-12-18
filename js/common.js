2e03b2be4c9b732e2c8b32eee6616b647c9d63bf
Pavel <zinovev-pasha@mail.ru>
2017-12-18 12:04:16 +0300

    Добавлена первая секция и меню

--- js/common.js
+++ js/common.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
 
-	
-	
+	heightDetect();
+
+	$(".toggle-mnu, .menu_item").click(function () {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".toggle-mnu").click(function () {
+		if($(".top_mnu").is(":visible")) {
+			$(".top_mnu").fadeOut(600);
+		} else {
+			$(".top_mnu").fadeIn(600);
+		}
+	});
 });
+
+$(window).load(function() {
+	$(".loaderInner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+});
+
+$(window).resize(function () {
+	heightDetect();
+});
+
+function heightDetect() {
+	$(".main-head").css("height", $(window).height());
+}

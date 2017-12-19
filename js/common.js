dc7ba0d1dc63fc46406926f8575332aa3f46de4e
Pavel <zinovev-pasha@mail.ru>
2017-12-19 11:28:20 +0300

    Доработка первой секции

--- js/common.js
+++ js/common.js
@@ -2,17 +2,24 @@ $(document).ready(function() {
 
 	heightDetect();
 
-	$(".toggle-mnu, .menu_item").click(function () {
+	$(".top_mnu>ul>li>a").click(function () {
+		$(".top_mnu").fadeOut(600);
 		$(".sandwich").toggleClass("active");
 	});
 
 	$(".toggle-mnu").click(function () {
+		$(".sandwich").toggleClass("active");
 		if($(".top_mnu").is(":visible")) {
+			$(".top_text").removeClass("h_opacify");
 			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
 		} else {
+			$(".top_text").addClass("h_opacify");
 			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
 		}
 	});
+
 });
 
 $(window).load(function() {

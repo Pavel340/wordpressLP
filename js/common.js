0aa4f2fa8a0f347834936d9ff5c5207c39c3f9ae
Pavel <zinovev-pasha@mail.ru>
2017-12-21 10:27:00 +0300

    Секция Обо мне

--- js/common.js
+++ js/common.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 
+	$(".popup").magnificPopup({
+		type: "image"
+	});
+
 	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
 	$(".top_text p, .section_header").animated("fadeInUp", "fadeOutDown");
+
+	$(".animation_1").animated("flipInY", "flipOutY");
+	$(".animation_2").animated("fadeInLeft", "fadeOutLeft");
+	$(".animation_3").animated("fadeInRight", "fadeOutRight");
 
 	heightDetect();
 

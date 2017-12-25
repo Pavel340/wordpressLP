d507e60c231857c2ace23199dafa41674895f28e
Pavel <zinovev-pasha@mail.ru>
2017-12-25 12:02:56 +0300

    Секция портфолио

--- js/common.js
+++ js/common.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 
-	$(".popup").magnificPopup({
-		type: "image"
+	$("#portfolio_grid").mixItUp();
+
+	$(".s_portfolio li").click(function() {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	$(".popup").magnificPopup({type: "image"});
+	$(".popup_content").magnificPopup({
+		type: "inline",
+		midClick: true
 	});
 
 	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
@@ -32,6 +41,11 @@ $(document).ready(function() {
 			$(".top_mnu").fadeIn(600);
 			$(".top_mnu li a").addClass("fadeInUp animated");
 		}
+	});
+
+	$(".portfolio_item").each(function(i){
+		$(this).find("a").attr("href", "#work_" + i);
+		$(this).find(".port_descr").attr("id", "work_" + i);
 	});
 
 });
